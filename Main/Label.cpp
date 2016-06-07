b38625a2fd317e7e0b8c4cd56f2359294d7e1642
Timo Denk <simssosdenk@googlemail.com>
2016-06-07 22:11:27 +0200

    Start game state and exclude Label class

--- Main/Label.cpp
+++ Main/Label.cpp
@@ -2,37 +2,79 @@
   #include "Adafruit_ILI9341.h"
 #endif
 
+// Issues: If text length changes for centered text the text is off center and the old text is not fully removed.
+
 class Label {
 public:
   Label()
-    : tft(0), x(0), y(0), color(ILI9341_WHITE), bgColor(ILI9341_BLACK), textSize(2), text("") {};
-  Label(Adafruit_ILI9341* tft, int x, int y, int color, int bgColor, int textSize, String text)
-    : tft(tft), x(x), y(y), color(color), bgColor(bgColor), textSize(textSize), text(text)
+    : tft(0), x(0), y(0), color(ILI9341_WHITE), bgColor(ILI9341_BLACK), textSize(2), text(""), oldTextLength(0) {};
+  Label(Adafruit_ILI9341* tft, int x, int y, int color, int bgColor, int textSize, String text, bool hidden)
+    : tft(tft), x(x), y(y), color(color), bgColor(bgColor), textSize(textSize), text(text), hidden(hidden)
   {
+    this->oldTextLength = this->text.length();
+    this->refresh();  
+  };
+
+  // center text
+  Label(Adafruit_ILI9341* tft, int y, int color, int bgColor, int textSize, String text, bool hidden)
+    : tft(tft), y(y), color(color), bgColor(bgColor), textSize(textSize), text(text), hidden(hidden)
+  {
+    this->oldTextLength = this->text.length();
+    int width = 6 * this->textSize * this->text.length();
+    this->x = (ILI9341_TFTHEIGHT - width) / 2;
     this->refresh();  
   };
 
   void setText(String newText) {
-    this->text = newText;
-    this->refresh();
+    if (this->text != newText) {
+      this->text = newText;
+      this->refresh();
+    }
   }
 
   void setColor(int newColor) {
     this->color = newColor;
     this->refresh();
   }
+
+  void hide() {
+    this->hidden = true;
+    this->refresh();
+  }
+
+  void show() {
+    this->hidden = false;
+    this->refresh();
+  }
+
+  int getHeight() const {
+    return 6 * this->textSize;
+  }
+
+  int getWidth() const {
+    return this->textSize * 8 * this->text.length();
+  }
+
+  int getRightX() const {
+    return this->getWidth() + this->x;
+  }
+
+  int getBottomY() const {
+    return this->getHeight() + this->y;
+  }
   
 private:
   Adafruit_ILI9341 *tft;
-  int x, y, color, bgColor, textSize;
+  int x, y, color, bgColor, textSize, oldTextLength;
   String text;
+  bool hidden;
 
   void refresh() {
     this->tft->setCursor(this->x, this->y);
     this->tft->setTextColor(this->color);
     this->tft->setTextSize(this->textSize);
       
-    for (int i = 0; i < this->text.length(); i++) {
+    for (unsigned int i = 0; i < this->text.length(); i++) {
       // remove the old text
       this->tft->fillRect(
         this->x + i * 6 * this->textSize, 
@@ -40,7 +82,10 @@ private:
         6 * this->textSize, // height of the text
         this->textSize * 8, // width of the text
         this->bgColor);
-      this->tft->print(this->text.charAt(i));
+
+      if (!this->hidden) {
+        this->tft->print(this->text.charAt(i));
+      }
     }
   }
 };
--- Main/Main.ino
+++ Main/Main.ino
@@ -7,10 +7,24 @@
 
 #define DEBUG
 
+enum GameState {
+  Start,
+  Running,
+  Over
+};
+
 // screen pinning
 const uint8_t TFT_RST = 8, TFT_DC = 9, TFT_CS = 10, TFT_MOSI = 11, TFT_MISO = 12, TFT_CLK = 13;
 
+const int BG_COLOR = ILI9341_BLACK;
+
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+Label *title, *subtitle, *penaltyTimeTitle, *penaltyTime;
+
+GameState gameState = GameState::Start;
+
+int penaltyTimeValue = 5; // 5 seconds by default
 
 void setup()
 {
@@ -20,16 +34,22 @@ void setup()
   #endif
   
   tft.begin();
+  tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
-  Label test(&tft, 50, 50, ILI9341_WHITE, ILI9341_BLACK, 2, "DJ Simsso");
+
+  title = new Label(&tft, 10, ILI9341_RED, BG_COLOR, 3, "KreativeKiste.de", false);
+  subtitle = new Label(&tft, title->getBottomY() + 10, ILI9341_RED, BG_COLOR, 2, "Heisser Draht", false);
+  penaltyTimeTitle = new Label(&tft, subtitle->getBottomY() + 30, ILI9341_WHITE, BG_COLOR, 2, "Strafzeit einstellen", true);
+  penaltyTime = new Label(&tft, penaltyTimeTitle->getBottomY() + 20, ILI9341_WHITE, BG_COLOR, 3, String(penaltyTimeValue) + " s", true);
   delay(2000);
-  test.setText("is in da house");
-  delay(2000);
-  test.setColor(ILI9341_YELLOW);
 }
 
 void loop()
 {
-  
+  penaltyTimeTitle->show();
+  penaltyTime->show();
+  while (gameState == GameState::Start) {
+    penaltyTime->setText(String(analogRead(A0)) + " s"); // updateLabel
+  }
 }
 
